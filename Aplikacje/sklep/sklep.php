a9cd3261c9841242c8be9381b423b11e808ffa6e
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-10-09 20:33:30 +0200

    Create sklep.php

---
+++ Aplikacje/sklep/sklep.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+	<meta charset="utf-8">
+		<title>Sklep</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id="glowny">
+			<div id="tytul">
+				<h2>Sklep</h2>
+			</div>
+			<div id="menu">
+				<h3>Menu</h3>
+				<a href="sklep.php">Home</a><br />
+				<a href="produkty.php">Produkty</a><br />
+				<a href="logowanie.php">Zaloguj</a><br />
+				<a href="rejestracja.php">Rejestracja</a><br />
+			</div>
+			<div id="srodek">
+				Logo
+				<img src="debil2.jpg">
+			</div>
+		</div>
+	</body>
+</html>
