614aab6d631c74835f28ed4e3c6b2f51f2b18dc7
Wojtek Frankiewicz <wojtekfrankiewicz8@gmail.com>
2022-10-09 20:34:53 +0200

    Template for .cpp files

---
+++ C++/szablon.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <conio.h>
+#include <time.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+	system("PAUSE");
+	return 0;	
+}
